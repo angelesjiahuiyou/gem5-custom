ff28bbd7a5095d45a82803014d6f39c208f80906
Tommaso Marinelli <tommarin@ucm.es>
2020-10-06 01:54:54 +0200

    Use python3 for everything

--- stats_processing/chain/after_step2.sh
+++ stats_processing/chain/after_step2.sh
@@ -20,15 +20,15 @@ python3 cpt_step5_pca.py step4/*
 python3 cpt_step6_regression.py step4/slowdown_*
 
 # Plots
-python plot_stuff.py step3/slowdown_int_sc-a53-odn2_stt-l2_typical.csv step3/slowdown_int_sc-a53-odn2_stt-l2_worst.csv
-python plot_stuff.py step3/slowdown_fp_sc-a53-odn2_stt-l2_typical.csv step3/slowdown_fp_sc-a53-odn2_stt-l2_worst.csv
-python plot_stuff.py step3/slowdown_int_sc-imec-a15-p0_stt-l2_typical.csv step3/slowdown_int_sc-imec-a15-p0_stt-l2_worst.csv
-python plot_stuff.py step3/slowdown_fp_sc-imec-a15-p0_stt-l2_typical.csv step3/slowdown_fp_sc-imec-a15-p0_stt-l2_worst.csv
-python plot_stuff.py step3/slowdown_int_sc-i7-6700_stt-l3_typical.csv step3/slowdown_int_sc-i7-6700_stt-l3_worst.csv
-python plot_stuff.py step3/slowdown_fp_sc-i7-6700_stt-l3_typical.csv step3/slowdown_fp_sc-i7-6700_stt-l3_worst.csv
-python plot_stuff.py step3/baseline_int_sc-a53-odn2_sram-only_typical.csv
-python plot_stuff.py step3/baseline_fp_sc-a53-odn2_sram-only_typical.csv
-python plot_stuff.py step3/baseline_int_sc-imec-a15-p0_sram-only_typical.csv
-python plot_stuff.py step3/baseline_fp_sc-imec-a15-p0_sram-only_typical.csv
-python plot_stuff.py step3/baseline_int_sc-i7-6700_sram-only_typical.csv
-python plot_stuff.py step3/baseline_fp_sc-i7-6700_sram-only_typical.csv
+python3 plot_stuff.py step3/slowdown_int_sc-a53-odn2_stt-l2_typical.csv step3/slowdown_int_sc-a53-odn2_stt-l2_worst.csv
+python3 plot_stuff.py step3/slowdown_fp_sc-a53-odn2_stt-l2_typical.csv step3/slowdown_fp_sc-a53-odn2_stt-l2_worst.csv
+python3 plot_stuff.py step3/slowdown_int_sc-imec-a15-p0_stt-l2_typical.csv step3/slowdown_int_sc-imec-a15-p0_stt-l2_worst.csv
+python3 plot_stuff.py step3/slowdown_fp_sc-imec-a15-p0_stt-l2_typical.csv step3/slowdown_fp_sc-imec-a15-p0_stt-l2_worst.csv
+python3 plot_stuff.py step3/slowdown_int_sc-i7-6700_stt-l3_typical.csv step3/slowdown_int_sc-i7-6700_stt-l3_worst.csv
+python3 plot_stuff.py step3/slowdown_fp_sc-i7-6700_stt-l3_typical.csv step3/slowdown_fp_sc-i7-6700_stt-l3_worst.csv
+python3 plot_stuff.py step3/baseline_int_sc-a53-odn2_sram-only_typical.csv
+python3 plot_stuff.py step3/baseline_fp_sc-a53-odn2_sram-only_typical.csv
+python3 plot_stuff.py step3/baseline_int_sc-imec-a15-p0_sram-only_typical.csv
+python3 plot_stuff.py step3/baseline_fp_sc-imec-a15-p0_sram-only_typical.csv
+python3 plot_stuff.py step3/baseline_int_sc-i7-6700_sram-only_typical.csv
+python3 plot_stuff.py step3/baseline_fp_sc-i7-6700_sram-only_typical.csv
